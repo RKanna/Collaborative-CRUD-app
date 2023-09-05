aa1acd63e0dd8ef2b6737e3c5a9a2c012a140226
RKanna <rkannan.balakrishnan@hotmail.com>
2023-09-05 16:54:21 +0530

    overlay manipulation

--- scripts/script.js
+++ scripts/script.js
@@ -13,21 +13,21 @@ const madebyDescription = document.getElementById("madeby");
 const priceDescription = document.getElementById("price-des");
 const imageDescription = document.getElementById("img-des");
 
-function generateUniqueIds() {
-  // Get all the row elements with class "row"
+// function generateUniqueIds() {
+//   // Get all the row elements with class "row"
 
-  // Loop through each row and generate a unique ID
-  rows.forEach((row) => {
-    const uniqueId = `row-${Date.now()}-${Math.random()
-      .toString(36)
-      .substring(7)}`;
-    row.setAttribute("data-row-id", uniqueId);
-    console.log(uniqueId);
-  });
-}
+//   // Loop through each row and generate a unique ID
+//   rows.forEach((row) => {
+//     const uniqueId = `row-${Date.now()}-${Math.random()
+//       .toString(36)
+//       .substring(7)}`;
+//     row.setAttribute("data-row-id", uniqueId);
+//     console.log(uniqueId);
+//   });
+// }
 
-// Call the function to generate unique IDs
-generateUniqueIds();
+// // Call the function to generate unique IDs
+// generateUniqueIds();
 
 function handleProductClick() {
   rows.forEach((row) => {
@@ -116,25 +116,101 @@ function homepageOverlay() {
   overlaySection.style.display = "none";
   heroPage.style.display = "flex";
 }
-const smallCartBtns = document.getElementsByClassName("btn-small");
+const smallCartBtns = document.querySelectorAll(".btn-small");
 const overlaySection = document.getElementById("overlay-section");
 
 overlaySection.style.display = "none";
+
+const overlayImage = document.getElementById("overlayimg");
+const overlayTitle = document.getElementById("overlayh2-title");
+const overlayPrice = document.getElementById("overlayprice");
 
 for (const smallCartBtn of smallCartBtns) {
   smallCartBtn.addEventListener("click", (event) => {
     // Prevent the click event from propagating to the parent "row" element.
     event.stopPropagation();
 
-    // Handle the click for each "btn-small" button here.
-    // You can add code specific to each small button's behavior.
-    // For example, adding the product associated with the clicked button to the cart.
+    // Access the specific button that was clicked using event.currentTarget.
+    const clickedButton = event.currentTarget;
 
-    // Access the specific button that was clicked using event.target.
-    const clickedButton = event.target;
-    overlaySection.style.display = "flex";
-    // You can now work with the clicked button, for example:
-    // const productId = clickedButton.getAttribute("data-product-id");
-    // Add the product to the cart or perform any other desired action.
+    // Get the productId from the clicked button's data-product-id attribute.
+    const productId = clickedButton.getAttribute("data-product-id");
+
+    // Handle the click for each "btn-small" button here.
+    switch (productId) {
+      case "1":
+        // Handle the "addToCart" behavior here.
+        overlayImage.src = "/images/product-1.png";
+        overlayTitle.innerText = "Google Pixel - Black";
+        overlayPrice.innerText = "Price : $ 10";
+        overlaySection.style.display = "flex";
+
+        // Add the product to the cart or perform any other desired action.
+        break;
+
+      case "2":
+        // Handle the "removeFromCart" behavior here.
+        overlayImage.src = "/images/product-2.png";
+        overlayTitle.innerText = "Samsung S7";
+        overlayPrice.innerText = "Price : $ 16";
+        overlaySection.style.display = "flex";
+
+        // You can remove the product associated with the clicked button from the cart.
+        break;
+      case "3":
+        // Handle the "removeFromCart" behavior here.
+        overlayImage.src = "/images/product-3.png";
+        overlayTitle.innerText = "HTC 10 - Black";
+        overlayPrice.innerText = "Price : $ 8";
+        overlaySection.style.display = "flex";
+
+        // You can remove the product associated with the clicked button from the cart.
+        break;
+      case "4":
+        // Handle the "removeFromCart" behavior here.
+        overlayImage.src = "/images/product-4.png";
+        overlayTitle.innerText = "HTC 10 - White";
+        overlayPrice.innerText = "Price : $ 18";
+        overlaySection.style.display = "flex";
+
+        // You can remove the product associated with the clicked button from the cart.
+        break;
+      case "5":
+        // Handle the "removeFromCart" behavior here.
+        overlayImage.src = "/images/product-5.png";
+        overlayTitle.innerText = "HTC Desire 626s";
+        overlayPrice.innerText = "Price : $ 24";
+        overlaySection.style.display = "flex";
+
+        // You can remove the product associated with the clicked button from the cart.
+        break;
+      case "6":
+        // Handle the "removeFromCart" behavior here.
+        overlayImage.src = "/images/product-6.png";
+        overlayTitle.innerText = "Vintage Iphone";
+        overlayPrice.innerText = "Price : $ 17";
+        overlaySection.style.display = "flex";
+
+        // You can remove the product associated with the clicked button from the cart.
+        break;
+      case "7":
+        // Handle the "removeFromCart" behavior here.
+        overlayImage.src = "/images/product-7.png";
+        overlayTitle.innerText = "Iphone 7";
+        overlayPrice.innerText = "Price : $ 30";
+        overlaySection.style.display = "flex";
+
+        // You can remove the product associated with the clicked button from the cart.
+        break;
+      case "8":
+        // Handle the "removeFromCart" behavior here.
+        overlayImage.src = "/images/product-8.png";
+        overlayTitle.innerText = "Smashed Iphone";
+        overlayPrice.innerText = "Price : $ 2";
+        overlaySection.style.display = "flex";
+
+        // You can remove the product associated with the clicked button from the cart.
+        break;
+    }
   });
 }
